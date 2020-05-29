75b92f490d9f90fcb3b8f58d5fb5033a76d8df8c
miedson <miedson.fernandes@hotmail.com>
2020-05-29 15:45:56 -0300

    melhorias na manipulação e layout do arquivo de saida e adição de mensagem no terminal ao encontrar um ocorrencia

--- search.py
+++ search.py
@@ -6,18 +6,18 @@ if(len(sys.argv) == 3):
     valor = sys.argv[1]
     tipo = sys.argv[2]
     path = os.path.dirname(__file__)
+    saida = open(path + '/saida.txt', 'w+')
     files = glob.glob(path + '/*.' + tipo)
     for file in files:
-        arq = open(file, 'r')
+        arq = open(file, 'r', encoding='ANSI')
         for txt in arq.readlines():
             if valor in txt:
-                saida = open(path + '/saida.txt', 'w+')
                 saida.writelines(f'Arquivo:\n {file} \n\nLinha:\n {txt}')
-                saida.close()
+                saida.writelines('=========================================\n')
+                print(f'Ocorrência encontrada no arquivo: {file}')
         arq.close()
-
-    print("Pesquisa concluida, arquivo com resultado encontra-se em: " +
-          path + '/saida.txt')
-
+    print(
+        f"Pesquisa concluida, arquivo com resultado encontra-se em: {path}/saida.txt")
+    saida.close()
 else:
     print("Falta passar o valor da pesquisa ou o tipo de arquivo após o script!")
