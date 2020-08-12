90ea0dea91af4e5b44e4d3aa6b97bb23818760e8
miedson <miedson.fernandes@hotmail.com>
2020-08-12 11:56:32 -0300

    Reescrita de quase todo o código para adição de pesquisa em subdiretórios

--- search.py
+++ search.py
@@ -1,23 +1,62 @@
 import sys
 import os
-import glob
+from datetime import datetime
 
 if(len(sys.argv) == 3):
     valor = sys.argv[1]
     tipo = sys.argv[2]
-    path = os.path.dirname(__file__)
-    saida = open(path + '/saida.txt', 'w+')
-    files = glob.glob(path + '/*.' + tipo)
-    for file in files:
-        arq = open(file, 'r', encoding='ANSI')
-        for txt in arq.readlines():
-            if valor in txt:
-                saida.writelines(f'Arquivo:\n {file} \n\nLinha:\n {txt}')
-                saida.writelines('=========================================\n')
-                print(f'Ocorrência encontrada no arquivo: {file}')
-        arq.close()
-    print(
-        f"Pesquisa concluida, arquivo com resultado encontra-se em: {path}/saida.txt")
-    saida.close()
+    path = os.path.abspath(__file__)[:os.path.abspath(__file__).rfind("\\") + 1]
+    
+    def insertDateTime(txt):
+        datetimeNow = datetime.now()
+        with open(f'{path}saida.txt', 'at') as arq:
+            arq.write(f"{txt}{datetimeNow.strftime('%d/%m/%Y %H:%M')}\n\n")
+            arq.close()
+
+    def openFile(pathvalidated, dirs):
+        count = 0
+        for pathValid in pathvalidated:
+            with open(pathValid, 'r', encoding='ANSI') as arq:
+                for txt in arq.readlines():
+                    if(valor in txt):
+                        with open(f'{path}saida.txt', 'at') as saida:
+                            saida.writelines(f'Local:\n {pathValid}\n\nLinha:\n {txt}')
+                            saida.writelines('=========================================\n')
+                            print(f'Ocorrência encontrada no arquivo: {pathValid}')
+                        count += 1
+                        saida.close()
+            arq.close()
+        if(count > 0):
+            print(f'Busca concluida, foram encontrados um total de {count} ocorrência(s), o resultado encontra-se em {path}saida.txt')
+            insertDateTime('\nFim: ')
+        else:
+            print(f'Não foi encontrado nenhuma ocorrência de {valor} nos diretorios: \n' + ', \n'.join(dirs))
+            insertDateTime(f'Não foi encontrado nenhuma ocorrência de {valor} nos diretorios: \n' + ', \n'.join(dirs) + '\n\nFim: ')
+
+    def verifyFile(files, dirs):
+        pathvalidated = []
+        for file in files:
+            for dir in dirs:
+                if os.path.exists(f'{dir}\{file}'):
+                    pathvalidated.append(f'{dir}\{file}')
+        openFile(pathvalidated, dirs)
+
+    def scanDir(path):
+        arr = []
+        dirs = []
+        if os.path.exists(f'{path}saida.txt'):
+            os.remove(f'{path}saida.txt')
+        insertDateTime('Inicio: ')
+        '''os.walk() lista tudo do diretorio em uma arvore de array separando arquivos de subdiretorios'''
+        for files in os.walk(path):
+            dirs.append(files[0])
+            for file in files[2]:
+                if(os.path.splitext(file)[1] == f'.{tipo}'):
+                    arr.append(file)
+        verifyFile(arr, dirs)
+
+
+    scanDir(path)
+
 else:
     print("Falta passar o valor da pesquisa ou o tipo de arquivo após o script!")
